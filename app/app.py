b8bd9873740c98e97d8c6e15af291c3bd2cca45b
ian <isk5@njit.edu>
2020-07-28 20:17:09 -0400

    update

--- app/app.py
+++ app/app.py
@@ -12,7 +12,7 @@ app.config['MYSQL_DATABASE_HOST'] = 'db'
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
 app.config['MYSQL_DATABASE_PORT'] = 3306
-app.config['MYSQL_DATABASE_DB'] = 'tblGradesImportData'
+app.config['MYSQL_DATABASE_DB'] = 'gradesData'
 mysql.init_app(app)
 
 
@@ -20,7 +20,7 @@ mysql.init_app(app)
 def index():
     user = {'username': 'Ssempax Project'}
     cursor = mysql.get_db().cursor()
-    cursor.execute('SELECT * FROM tblGradesImport')
+    cursor.execute('SELECT * FROM grades')
     result = cursor.fetchall()
     return render_template('index.html', title='Home', user=user, grades=result)
 
@@ -28,7 +28,7 @@ def index():
 @app.route('/view/<int:grade_id>', methods=['GET'])
 def record_view(grade_id):
     cursor = mysql.get_db().cursor()
-    cursor.execute('SELECT * FROM tblGradesImport WHERE id=%s', grade_id)
+    cursor.execute('SELECT * FROM grades WHERE id=%s', grade_id)
     result = cursor.fetchall()
     return render_template('view.html', title='View Form', grade=result[0])
 
@@ -36,7 +36,7 @@ def record_view(grade_id):
 @app.route('/edit/<int:grade_id>', methods=['GET'])
 def form_edit_get(grade_id):
     cursor = mysql.get_db().cursor()
-    cursor.execute('SELECT * FROM tblGradesImport WHERE id=%s', grade_id)
+    cursor.execute('SELECT * FROM grades WHERE id=%s', grade_id)
     result = cursor.fetchall()
     return render_template('edit.html', title='Edit Form', grade=result[0])
 
@@ -48,26 +48,26 @@ def form_update_post(grade_id):
                  request.form.get('Test1'), request.form.get('Test2'),
                  request.form.get('Test3'), request.form.get('Test4'),
                  request.form.get('Final'), request.form.get('Grade'), grade_id)
-    sql_update_query = """UPDATE tblGradesImport t SET t.Last_name = %s, t.First_name = %s, t.SSN = %s, t.Test1 = 
+    sql_update_query = """UPDATE grades t SET t.Last_name = %s, t.First_name = %s, t.SSN = %s, t.Test1 = 
     %s, t.Test2 = %s, t.Test3 = %s, t.Test4 = %s ,t.Final = %s, t.Grade =%s   WHERE t.id = %s """
     cursor.execute(sql_update_query, inputData)
     mysql.get_db().commit()
     return redirect("/", code=302)
 
 
-@app.route('/tblGradesImport/new', methods=['GET'])
+@app.route('/grades/new', methods=['GET'])
 def form_insert_get():
-    return render_template('new.html', title='New tblGradesImport Form')
+    return render_template('new.html', title='New grades Form')
 
 
-@app.route('/tblGradesImport/new', methods=['POST'])
+@app.route('/grades/new', methods=['POST'])
 def form_insert_post():
     cursor = mysql.get_db().cursor()
     inputData = (request.form.get('Last_name'), request.form.get('First_name'), request.form.get('SSN'),
                  request.form.get('Test1'), request.form.get('Test2'),
                  request.form.get('Test3'), request.form.get('Test4'),
                  request.form.get('Final'), request.form.get('Grade'))
-    sql_insert_query = """INSERT INTO tblGradesImport (Last_name,First_name,Test1,Test2,Test3,Test4,Final, Grade) VALUES (%s, %s,
+    sql_insert_query = """INSERT INTO grades (Last_name,First_name,Test1,Test2,Test3,Test4,Final, Grade) VALUES (%s, %s,
     %s, %s,%s, %s,%s, %s) """
     cursor.execute(sql_insert_query, inputData)
     mysql.get_db().commit()
@@ -77,33 +77,33 @@ def form_insert_post():
 @app.route('/delete/<int:grade_id>', methods=['POST'])
 def form_delete_post(grade_id):
     cursor = mysql.get_db().cursor()
-    sql_delete_query = """DELETE FROM tblGradesImport WHERE id = %s """
+    sql_delete_query = """DELETE FROM grades WHERE id = %s """
     cursor.execute(sql_delete_query, grade_id)
     mysql.get_db().commit()
     return redirect("/", code=302)
 
 
-@app.route('/api/v1/tblGradesImport', methods=['GET'])
+@app.route('/api/v1/grades', methods=['GET'])
 def api_browse() -> str:
     cursor = mysql.get_db().cursor()
-    cursor.execute('SELECT * FROM tblGradesImport')
+    cursor.execute('SELECT * FROM grades')
     result = cursor.fetchall()
     json_result = json.dumps(result);
     resp = Response(json_result, status=200, mimetype='application/json')
     return resp
 
 
-@app.route('/api/v1/tblGradesImport/<int:grade_id>', methods=['GET'])
+@app.route('/api/v1/grades/<int:grade_id>', methods=['GET'])
 def api_retrieve(grade_id) -> str:
     cursor = mysql.get_db().cursor()
-    cursor.execute('SELECT * FROM tblGradesImport WHERE id=%s', grade_id)
+    cursor.execute('SELECT * FROM grades WHERE id=%s', grade_id)
     result = cursor.fetchall()
     json_result = json.dumps(result);
     resp = Response(json_result, status=200, mimetype='application/json')
     return resp
 
 
-@app.route('/api/v1/tblGradesImport/', methods=['POST'])
+@app.route('/api/v1/grades/', methods=['POST'])
 def api_add() -> str:
     resp = Response(status=201, mimetype='application/json')
     return resp
